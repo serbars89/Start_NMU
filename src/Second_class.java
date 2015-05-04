a82743ca68050de598c31e09d114bc898b181083
serbars89 <velikiybarsovich@gmail.com>
2015-05-04 18:41:40 +0300

    add new two fales

---
+++ src/Second_class.java
@@ -0,0 +1,11 @@
+/**
+ * Created by SER on 04.05.2015.
+ */
+import java.lang.System;
+import java.lang.*;
+
+public class Second_class {
+
+    public void print()
+    {System.out.println("а ведь реально пушу!!! ");}
+}
---
+++ src/Super_java.java
@@ -0,0 +1,10 @@
+/**
+ * Created by SER on 04.05.2015.
+ */
+public class Super_java {
+    public static void main(String[] args) {
+        System.out.println("Я пушу свой первыый проект!! ");
+        Second_class sec = new Second_class();
+        sec.print();
+    }
+}
